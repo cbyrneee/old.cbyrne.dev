032aed7c9e2202cb0a9555c24f4127d6ec1be452
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-06-22 21:32:12 +0100

    Add a scroll button & start projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import React from "react"
 import Button from "../components/Button"
 import styles from "../styles/Landing.module.css"
 
+import { Link } from "react-scroll"
+
 export default function Landing() {
     return (
-        <div className={styles.landingContainer}>
+        <>
             <Head>
                 <title>Conor Byrne</title>
                 <meta
@@ -15,27 +17,52 @@ export default function Landing() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Conor Byrne</h1>
+            <div className={styles.landingContainer}>
+                <main className={styles.main}>
+                    <h1 className={styles.title}>Conor Byrne</h1>
 
-                <p className={styles.subtitle}>
-                    Java, Kotlin and Web Developer
-                </p>
+                    <p className={styles.subtitle}>
+                        Java, Kotlin and Web Developer
+                    </p>
 
-                <div className={styles.buttonContainer}>
-                    <Button
-                        name={"GitHub"}
-                        icon={["fab", "github"]}
-                        destination={"https://github.com/cbyrneee"}
-                    />
-                    <Button
-                        name={"Contact"}
-                        icon={["fas", "envelope"]}
-                        destination={"mailto:hello@cbyrne.dev"}
-                    />
-                </div>
-            </main>
-        </div>
+                    <div className={styles.buttonContainer}>
+                        <Button
+                            name={"GitHub"}
+                            icon={["fab", "github"]}
+                            destination={"https://github.com/cbyrneee"}
+                        />
+                        <Button
+                            name={"Contact"}
+                            icon={["fas", "envelope"]}
+                            destination={"mailto:hello@cbyrne.dev"}
+                        />
+                    </div>
+                </main>
+
+                <Link
+                    className={styles.scrollIndicator}
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    duration={1000}
+                >
+                    <svg
+                        className={styles.scrollImage}
+                        width="30"
+                        height="19"
+                        viewBox="0 0 30 19"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M26.475 0.474976L15 11.925L3.525 0.474976L0 3.99997L15 19L30 3.99997L26.475 0.474976Z" />
+                    </svg>
+                </Link>
+            </div>
+
+            <div id={"projects"} className={styles.projects}>
+                <p className={styles.subtitle}>Projects</p>
+            </div>
+        </>
     )
 }
 
