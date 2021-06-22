819e663d6ef2c11db7818c965e55cf073650c8a9
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-06-22 12:46:21 +0100

    [Style] Fix navigation bar on light theme

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -9,7 +9,7 @@ export default function Navigation() {
     return (
         <div className="bg-gray-200 dark:bg-gray-900 mx-auto py-2 pb-8 text-white">
             <div className="flex flex-flow-col gap-8 self-center">
-                <p className="font-semibold">Conor Byrne</p>
+                <p className="font-semibold text-black dark:text-white">Conor Byrne</p>
                 <Link href="/">
                     <a className={isHome ? "selected-link" : "unselected-link"}>
                         Home
