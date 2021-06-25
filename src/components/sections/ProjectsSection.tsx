099f2d7251b88422f21b2db49d570e0392e0be2e
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-06-25 18:56:08 +0100

    Layout changes and start working on projects

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,6 +1,22 @@
 import React from "react"
-import styles from "../../styles/Landing.module.css"
+import styles from "../../styles/Projects.module.css"
+
+function ProjectCard() {
+    return <div></div>
+}
 
 export default function ProjectsSection() {
-    return <div id={"projects"} className={styles.projects}></div>
+    return (
+        <div id={"projects"} className={styles.projectsContainer}>
+            <div className={styles.main}>
+                <h2 className={styles.title}>Projects</h2>
+                <p className={styles.subtitle}>
+                    A collection of my various projects that I have made
+                </p>
+                <div className={styles.cards}>
+                    <ProjectCard />
+                </div>
+            </div>
+        </div>
+    )
 }
