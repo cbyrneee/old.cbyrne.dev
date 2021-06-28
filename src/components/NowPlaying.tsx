8fc7a87f64d72050d883c3a81f2dbf0df0d3a4db
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-06-28 22:07:23 +0100

    Remove unused import in NowPlaying.tsx

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef, useState } from "react"
+import React from "react"
 import { Song } from "../lib/Song"
 import styles from "../styles/NowPlaying.module.css"
 
