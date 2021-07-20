a1e9a6c78e1b8200bbb84182762fbe9aeff0a2c7
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-07-20 17:21:22 +0100

    Format code

--- src/components/sections/MainSection.tsx
+++ src/components/sections/MainSection.tsx
@@ -53,17 +53,19 @@ export default function MainSection() {
                 duration={1000}
             >
                 <div className="flex flex-col text-center items-center">
-                    <p className="pb-6 dark:text-gray-200 opacity-0 lg:opacity-100">Scroll to <strong>Projects</strong></p>
+                    <p className="pb-6 dark:text-gray-200 opacity-0 lg:opacity-100">
+                        Scroll to <strong>Projects</strong>
+                    </p>
                     <svg
-                    className={styles.scrollImage}
-                    width="30"
-                    height="19"
-                    viewBox="0 0 30 19"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M26.475 0.474976L15 11.925L3.525 0.474976L0 3.99997L15 19L30 3.99997L26.475 0.474976Z" />
-                </svg>
+                        className={styles.scrollImage}
+                        width="30"
+                        height="19"
+                        viewBox="0 0 30 19"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M26.475 0.474976L15 11.925L3.525 0.474976L0 3.99997L15 19L30 3.99997L26.475 0.474976Z" />
+                    </svg>
                 </div>
             </Link>
         </div>
