4f0b7799cf2f8ba4796b2923c5edd416e6c9bef5
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-11-21 10:03:48 +0000

    Add /api/track endpoint

---
+++ src/pages/api/track.ts
@@ -0,0 +1,24 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    // Fetch the latest track for a user on Last.FM
+    const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LAST_FM_USER}&api_key=${process.env.LAST_FM_KEY}&format=json`)
+    if (!response.ok)
+        return res.status(response.status).json({message: "Error: Received non OK code from Last.FM!"})
+
+    const json = await response.json()
+    const currentTrack = json["recenttracks"]["track"][0]
+    if (!currentTrack || !currentTrack["@attr"]["nowplaying"])
+        return res.status(204).json({message: "Error: Received no track from Last.FM"})
+
+    const data = {
+        name: currentTrack["name"],
+        artist: currentTrack["artist"]["#text"],
+        image: currentTrack["image"][3]["#text"]
+    }
+
+    res.status(200).json(data)
+}
