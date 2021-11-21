29eb1079de17892fce67baa1c5c8d1971a724bd8
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-11-21 18:44:38 +0000

    Is this what you wanted..?

--- src/components/window/WindowButton.tsx
+++ src/components/window/WindowButton.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import useSound from "use-sound";
 
 interface WindowButtonProps {
     color: string;
 }
 
 function WindowButton(props: WindowButtonProps) {
-    return <div className={`bg-${props.color}-500 hover:bg-${props.color}-500/50 transition-colors rounded-xl w-5 h-5`}/>
+    const [play] = useSound("/vine.mp3");
+    return <button onClick={() => {
+        play()
+    }} className={`bg-${props.color}-500 hover:bg-${props.color}-500/50 transition-colors rounded-xl w-5 h-5`}/>
 }
 
 export default WindowButton;
