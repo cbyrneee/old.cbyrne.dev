68ea5d88a884e6bfa025df8c8a7ad0826ee11f33
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-11-21 12:58:33 +0000

    Add opengraph and twitter meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,27 @@ export default class AppDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="theme-color" content="#F8D0CD"/>
+                    <meta name="title" content="Conor Byrne - Professional Developer"/>
+                    <meta
+                        name="description"
+                        content="I am a developer based in Ireland with experience in Kotlin, Java and Typescript. Currently working with OPLegends!"
+                    />
+
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:url" content="https://cbyrne.dev/"/>
+                    <meta property="og:title" content="Conor Byrne - Professional Developer"/>
+                    <meta
+                        property="og:description"
+                        content="I am a developer based in Ireland with experience in Kotlin, Java and Typescript. Currently working with OPLegends!"
+                    />
+
+                    <meta property="twitter:card" content="summary"/>
+                    <meta property="twitter:url" content="https://cbyrne.dev/"/>
+                    <meta property="twitter:title" content="Conor Byrne - Professional Developer"/>
+                    <meta
+                        property="twitter:description"
+                        content="I am a developer based in Ireland with experience in Kotlin, Java and Typescript. Currently working with OPLegends!"
+                    />
                 </Head>
                 <body>
                 <Main/>
