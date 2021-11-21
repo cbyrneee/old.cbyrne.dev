3968ea547d940a36f0d4a944c62273f7bb39efff
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-11-21 18:24:26 +0000

    Better formatting in TooltipLinkButton.tsx

--- src/components/control/TooltipLinkButton.tsx
+++ src/components/control/TooltipLinkButton.tsx
@@ -15,23 +15,24 @@ interface TooltipLinkButtonProps {
 export default function TooltipLinkButton(
     {href, tooltip, dontOpenNewTab = false, inactive = false, children}: TooltipLinkButtonProps
 ) {
-    return <>
-        <Link href={href} passHref={true}>
-            <div
-                className={`${tooltip && `${styles.hasTooltip} group flex flex-col items-center`}`}>
-                {tooltip && <span className={`${styles.tooltip} group-hover:md:opacity-100 cursor-default`}>{tooltip}</span>}
+    return <Link href={href} passHref={true}>
+        <div
+            className={`${tooltip && `${styles.hasTooltip} group flex flex-col items-center`}`}
+        >
+            {tooltip &&
+            <span className={`${styles.tooltip} group-hover:md:opacity-100 cursor-default`}>{tooltip}</span>
+            }
 
-                <a
-                    className={"cursor-pointer"}
-                    style={{pointerEvents: (inactive && tooltip) ? "auto" : "none"}}
-                    target={dontOpenNewTab ? "_self" : "_blank"}
-                    referrerPolicy={"no-referrer"}
-                >
-                    <Button inactive={inactive}>
-                        {children}
-                    </Button>
-                </a>
-            </div>
-        </Link>
-    </>
+            <a
+                className={"cursor-pointer"}
+                style={{pointerEvents: (inactive && tooltip) ? "auto" : "none"}}
+                target={dontOpenNewTab ? "_self" : "_blank"}
+                referrerPolicy={"no-referrer"}
+            >
+                <Button inactive={inactive}>
+                    {children}
+                </Button>
+            </a>
+        </div>
+    </Link>
 }
